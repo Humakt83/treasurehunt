4f93fd411d5417a54f62a0a949fe087a49f13d09
Humakt83 <urlact@yahoo.com>
2019-07-21 22:39:10 +0300

    Initial version of treasure hunt

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Game from './Game';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { players: ['Player 1', 'Player 2']};
+    this.startGame = this.startGame.bind(this);
+    this.addPlayer = this.addPlayer.bind(this);
+    this.changeName = this.changeName.bind(this);
+  }
+
+  startGame() {
+    this.setState({players: this.state.players, gameStarted: true});
+  }
+
+  addPlayer() {
+    const players = this.state.players;
+    players.push('');
+    this.setState({players});
+  }
+
+  changeName(event, index) {
+    const players = this.state.players;    
+    players[index] = event.target.value;
+    this.setState({players});
+  }
+
+  render() {
+    if (this.state.gameStarted) {
+      return <Game />
+    }
+    let playerNames = this.state.players.map((item, index) => <input key={index} value={item} onChange={(value) => this.changeName(value, index)}></input>);    
+    return (
+
+      <div className="App">
+        <header className="App-header">
+          <span role="img" className="App-logo-money" aria-label="money">💶</span>
+          <span role="img" className="App-logo-money" aria-label="money">💰</span>
+          <span role="img" className="App-logo" aria-label="treasure">💎</span>
+          <span role="img" className="App-logo-money" aria-label="money">💰</span>
+          <span role="img" className="App-logo-money" aria-label="money">💶</span>
+        </header>
+        <div>
+          <button onClick={this.addPlayer}>Add player</button>
+          <button onClick={this.startGame} disabled={this.state.players.length <2}>Start game</button>
+          {playerNames}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Game.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Game extends React.Component {
+  render() {
+    return <span>Game started</span>
+  }
+}
+
+export default Game;
