7c6bb6231a5823f3483a3c20457f9ce38f4955db
Humakt83 <urlact@yahoo.com>
2020-08-19 00:44:10 +0300

    Put focus on the button on encounterModal

--- src/EncounterModal.js
+++ src/EncounterModal.js
@@ -3,6 +3,10 @@ import './EncounterModal.scss';
 
 export default class EncounterModal extends React.Component {
 
+  componentDidMount() {
+    this.confirmButton.focus();
+  }
+
   render() {
     const emoji = this.props.encounter.emoji ? <p className="modal-emoji">{this.props.encounter.emoji}</p> : '';
     return (
@@ -11,7 +15,12 @@ export default class EncounterModal extends React.Component {
           <div className="modal">
             {emoji}
             <p>{this.props.encounter.message}</p>
-            <button className="confirmButton" onClick={this.props.encounter.continue}>OK</button>
+            <button 
+              ref={(button) => this.confirmButton = button}
+              className="confirmButton"
+              onClick={this.props.encounter.continue}>
+              OK
+            </button>
           </div>
         </div>
       </section>
